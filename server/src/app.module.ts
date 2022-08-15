742395ab25fed9b7a8cc06b0c854b03cb356828e
Aayush Naik <aayushnai@gmail.com>
2022-08-16 00:51:36 +0530

    feat: typeorm and config setup

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { getEnvPath } from './common/helper/env.helper';
 import { RoomsModule } from './rooms/rooms.module';
+import { TypeOrmConfigService } from './shared/typeorm/typeorm.service';
+
+const envFilePath: string = getEnvPath(`${__dirname}/common/envs`);
 
 @Module({
-  imports: [RoomsModule],
+  imports: [
+    ConfigModule.forRoot({ envFilePath, isGlobal: true }),
+    TypeOrmModule.forRootAsync({ useClass: TypeOrmConfigService }),
+    RoomsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
