bfc310dd69cba3794212bad697182e27a701f8db
Aayush Naik <aayushnai@gmail.com>
2022-08-17 22:41:20 +0530

    feat: google oauth

--- webapp/src/app/app.module.ts
+++ webapp/src/app/app.module.ts
@@ -2,21 +2,32 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { environment } from 'src/environments/environment';
+
 import { AppComponent } from './app.component';
 import { OauthComponent } from './components/oauth/oauth.component';
 import { LoginComponent } from './screens/login/login.component';
+import { LoaderComponent } from './components/loader/loader.component';
+import { LoadingPipe } from './pipes/loading.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     OauthComponent,
-    LoginComponent
+    LoginComponent,
+    LoaderComponent,
+    LoadingPipe,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireAuthModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- webapp/src/app/screens/login/login.component.ts
+++ webapp/src/app/screens/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { Md5 } from 'ts-md5';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import firebase from 'firebase/compat/app';
 
 @Component({
   selector: 'app-login',
@@ -9,7 +11,7 @@ import { Md5 } from 'ts-md5';
 })
 export class LoginComponent implements OnInit {
   url: string;
-  constructor() {
+  constructor(public auth: AngularFireAuth) {
     const state = Md5.hashStr(Date.now().toString());
     this.url =
       environment.squareBasePath +
@@ -20,5 +22,14 @@ export class LoginComponent implements OnInit {
       state;
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    console.log(this.auth.user);
+  }
+
+  login() {
+    this.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+  }
+  logout() {
+    this.auth.signOut();
+  }
 }
